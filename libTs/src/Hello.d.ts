52de33eb4bdf344a1e0d6fb033294cba33f1c045
Olivier Kastel <olivier.kastel@gmail.com>
2017-05-18 09:40:26 -0700

    build

--- libTs/src/Hello.d.ts
+++ libTs/src/Hello.d.ts
@@ -3,5 +3,6 @@ export interface HelloProps {
     text?: string;
 }
 export default class Hello extends React.Component<HelloProps, void> {
+    componentDidMount(): void;
     render(): JSX.Element;
 }
--- libTs/src/Hello.jsx
+++ libTs/src/Hello.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import styles from './styles';
 export default class Hello extends React.Component {
+    componentDidMount() {
+        console.log('hello World');
+    }
     render() {
         const { text } = this.props;
         return (<div style={styles.div}>
-        Prejkwehfkjhfix
         {text || 'Hay.'}
       </div>);
     }
